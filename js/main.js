ae48cde044fa754e05425e55e422cd0f27d30db9
Ilo <c_ilo@tss-hr420-dm21.local>
2019-01-14 16:19:30 -0500

    javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(() => {
+	// self-invoking anonymous function (or Lambda)
+	// 
+	// var => means create a variable
+	// hatever comes after the = is what the variable ref
+	var images = document.querySelectorAll('img');
+
+	// document is always the webpage
+	// after the dot is always a method or property
+
+	console.log(images)
+})();
